40bcf824a7716b4b391ba3be366c3beeb9211066
Max Wood <knotri@gmail.com>
2024-12-08 13:50:02 -0700

    move everything to docker

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
 # Replace 'localhost' and '27017' with your MongoDB connection details if necessary
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://mongodb:27017")
 DATABASE_NAME = "payments_db"
 
 # Create an instance of AsyncIOMotorClient and connect to the database
